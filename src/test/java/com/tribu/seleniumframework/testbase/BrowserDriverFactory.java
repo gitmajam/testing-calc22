0c7b4b65a703213b9eb341ad583f70d4553a7339
Marlon Muñoz <marlonjavi@gmail.com>
2021-10-14 22:53:55 -0500

    Try WebDriverManager dependency

--- src/test/java/com/tribu/seleniumframework/testbase/BrowserDriverFactory.java
+++ src/test/java/com/tribu/seleniumframework/testbase/BrowserDriverFactory.java
@@ -4,6 +4,7 @@ import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BrowserDriverFactory {
 
@@ -12,27 +13,30 @@ public class BrowserDriverFactory {
 	 * is like a dictionary
 	 */
 	//private static ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
-	public static WebDriver driver;
+	static WebDriver driver;
 	public void createDriver(String browser, Logger log) {
 		browser.toLowerCase();
 
 		switch (browser) {
 		case "chrome":
-			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+			//System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
 			//driver.set(new ChromeDriver());
+			WebDriverManager.chromedriver().setup();
 			driver=new ChromeDriver();
 			break;
 
 		case "firefox":
-			System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
+			//System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
 			//driver.set(new FirefoxDriver());
+			WebDriverManager.chromedriver().setup();
 			driver=new FirefoxDriver();
 			break;
 
 		default:
 			System.out.println("Do not know how to start: " + browser + ", starting chrome.");
-			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+			//System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
 			//driver.set(new ChromeDriver());
+			WebDriverManager.chromedriver().setup();
 			driver=new ChromeDriver();
 			break;
 		}
