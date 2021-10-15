525a060f5c1b7b793b6e7eb924dc72b48893f13f
Marlon Muñoz <marlonjavi@gmail.com>
2021-10-15 01:33:25 -0500

    install chromdriver 94.0.4606.61

--- src/test/java/com/tribu/seleniumframework/testbase/BrowserDriverFactory.java
+++ src/test/java/com/tribu/seleniumframework/testbase/BrowserDriverFactory.java
@@ -10,12 +10,12 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BrowserDriverFactory {
-	
+
 	public static ChromeOptions getChromeOptions() {
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--test-type");
 		options.addArguments("disable-infobars");// disabling infobars
-		//options.addArguments("--disable-gpu");// applicable to windows os only
+		// options.addArguments("--disable-gpu");// applicable to windows os only
 		options.addArguments("--disable-popup-blocking");
 		options.addArguments("--disable-extensions"); // disabling extensions
 		options.addArguments("start-maximised");// open Browser in maximized mode
@@ -31,36 +31,40 @@ public class BrowserDriverFactory {
 	 * static variable that relates an specific webdriver instance with a thread, it
 	 * is like a dictionary
 	 */
-	//private static ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
+	// private static ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
 	static WebDriver driver;
+
 	public void createDriver(String browser, Logger log) {
 		browser.toLowerCase();
 
 		switch (browser) {
 		case "chrome":
-			//System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
-			//driver.set(new ChromeDriver());
+			// System.setProperty("webdriver.chrome.driver",
+			// "src/main/resources/chromedriver");
+			// driver.set(new ChromeDriver());
 			WebDriverManager.chromedriver().setup();
-			driver=new ChromeDriver(getChromeOptions());
+			driver = new ChromeDriver(getChromeOptions());
 			break;
 
 		case "firefox":
-			//System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
-			//driver.set(new FirefoxDriver());
-			WebDriverManager.chromedriver().setup();
-			driver=new FirefoxDriver();
+			// System.setProperty("webdriver.gecko.driver",
+			// "src/main/resources/geckodriver");
+			// driver.set(new FirefoxDriver());
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
 			break;
 
 		default:
 			System.out.println("Do not know how to start: " + browser + ", starting chrome.");
-			//System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
-			//driver.set(new ChromeDriver());
+			// System.setProperty("webdriver.chrome.driver",
+			// "src/main/resources/chromedriver");
+			// driver.set(new ChromeDriver());
 			WebDriverManager.chromedriver().setup();
-			driver=new ChromeDriver();
+			driver = new ChromeDriver();
 			break;
 		}
 		// Create driver
-		//log.info(driver.get().hashCode() + " Factory crea driver: ");
+		// log.info(driver.get().hashCode() + " Factory crea driver: ");
 		log.info(" Factory crea driver: ");
 	}
 
