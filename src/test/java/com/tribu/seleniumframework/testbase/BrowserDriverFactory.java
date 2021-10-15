4e94586fadcb4e07a0c02908ae3d3ba7a9b45cfd
Marlon Muñoz <marlonjavi@gmail.com>
2021-10-14 22:13:41 -0500

    Without thread locals variables

--- src/test/java/com/tribu/seleniumframework/testbase/BrowserDriverFactory.java
+++ src/test/java/com/tribu/seleniumframework/testbase/BrowserDriverFactory.java
@@ -11,34 +11,38 @@ public class BrowserDriverFactory {
 	 * static variable that relates an specific webdriver instance with a thread, it
 	 * is like a dictionary
 	 */
-	private static ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
-
+	//private static ThreadLocal<WebDriver> driver = new ThreadLocal<WebDriver>();
+	private static WebDriver driver;
 	public void createDriver(String browser, Logger log) {
 		browser.toLowerCase();
 
 		switch (browser) {
 		case "chrome":
 			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
-			driver.set(new ChromeDriver());
+			//driver.set(new ChromeDriver());
+			driver=new ChromeDriver();
 			break;
 
 		case "firefox":
 			System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
-			driver.set(new FirefoxDriver());
+			//driver.set(new FirefoxDriver());
+			driver=new FirefoxDriver();
 			break;
 
 		default:
 			System.out.println("Do not know how to start: " + browser + ", starting chrome.");
 			System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
-			driver.set(new ChromeDriver());
+			//driver.set(new ChromeDriver());
+			driver=new ChromeDriver();
 			break;
 		}
 		// Create driver
-		log.info(driver.get().hashCode() + " Factory crea driver: ");
+		//log.info(driver.get().hashCode() + " Factory crea driver: ");
+		log.info(" Factory crea driver: ");
 	}
 
 	/** this method returns the driver related with the current thread */
 	public WebDriver getDriver() {
-		return driver.get();
+		return driver;
 	}
 }
--- src/test/java/com/tribu/seleniumframework/testbase/TestBase.java
+++ src/test/java/com/tribu/seleniumframework/testbase/TestBase.java
@@ -12,22 +12,24 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
-public class TestBase extends TestUtilities{
+public class TestBase extends TestUtilities {
 
 	protected Logger log;
 	protected BrowserDriverFactory factory;
 
-
 	@BeforeTest(alwaysRun = true)
-	
-	/** create a Browser driver factory it lets run several test in parallel*/
+
+	/** create a Browser driver factory it lets run several test in parallel */
 	public void createBrowserFactory(ITestContext ctx) {
-		
-		/* this lines create a new instance of a logger to write out the status during the script execution */
+
+		/*
+		 * this lines create a new instance of a logger to write out the status during
+		 * the script execution
+		 */
 		this.testName = ctx.getCurrentXmlTest().getName();
 		this.testSuiteName = ctx.getSuite().getName();
 		log = LogManager.getLogger(testName);
-		
+
 		log.info("Se crea factory");
 		factory = new BrowserDriverFactory();
 	}
@@ -37,9 +39,9 @@ public class TestBase extends TestUtilities{
 	public void setUp(Method method, @Optional("chrome") String browser) {
 		/* this variable isn't used */
 		this.testMethodName = method.getName();
-		
+
 		log.info("Se ejecuta setup() ");
-		
+
 		/* factory creates a new instance of webdriver "browser" */
 		factory.createDriver(browser, log);
 		factory.getDriver().manage().window().maximize();
